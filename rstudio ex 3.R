369164e3f4da2ee76232fa9b992977fda4353999
omair-khan <31222025+omair-khan@users.noreply.github.com>
2017-10-15 14:05:45 -0500

    ex 3 ok
    setup

---
+++ rstudio ex 3.R
@@ -0,0 +1,10 @@
+#number 3 
+#setting up
+library(tidyverse)
+setwd('~/GitHub/sql-homework-assignment-omair-ryan-and-dylan/r)
+# ex. 3
+average_mass <-  250
+number <- 3
+biomass <-average_mass*number
+kg_biomass <- biomass/1000
+print(kg_biomass)
